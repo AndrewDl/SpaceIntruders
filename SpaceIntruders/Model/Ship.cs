7ea3de5cd2d8b4350aa0a2d6c0d1d13228b11aa1
AndrewDl <dlandrew@ukr.net>
2017-10-12 23:10:59 +0300

    First commit

---
+++ SpaceIntruders/Model/Ship.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SpaceIntruders.Model 
+{
+    class Ship : INotifyPropertyChanged
+    {
+        
+        private string name = "Untitled";
+        private int hp = 0;
+
+        private string imageURL = "";
+        private double width = 32;
+        private double height = 32;
+        private int x = 0;
+        private int y = 0;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Ship(string image, int x, int y, string Name, int HP)
+        {
+            imageURL = image;
+            this.x = x;
+            this.y = y;
+            this.HP = hp;
+            this.Name = Name;
+        }
+
+        public Ship()
+        {
+        }
+
+        public int X { get { return x; } set
+            {
+                x = value;
+                NotifyPropertyChanged("X");
+                NotifyPropertyChanged("Margin");
+            }
+        }
+        public int Y
+        {
+            get
+            {
+                return y;
+            }
+            set
+            {
+                y = value;
+                NotifyPropertyChanged("Y");
+                NotifyPropertyChanged("Margin");
+            }
+        }
+        public double Width { get { return width; } }
+        public double Height { get { return height; } }
+        public string ImageSouce { get { return imageURL; } }
+
+        public Thickness Margin
+        {
+            get
+            {
+                return new Thickness(x,y,0,0);
+            }
+        }
+
+        public int HP
+        {
+            get
+            {
+                return hp;
+            }
+            set
+            {
+                hp = (value >= 0) ? value : 0;
+                NotifyPropertyChanged("HP");
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+                NotifyPropertyChanged("Name");
+            }
+        }
+
+        public void NotifyPropertyChanged(string PropertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new System.ComponentModel.PropertyChangedEventArgs(PropertyName));
+            }
+        }
+    }
+}
---
+++ SpaceIntruders/ViewModel/SimpleViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SpaceIntruders.Model;
+using System.Collections.ObjectModel;
+using System.Timers;
+using System.ComponentModel;
+
+namespace SpaceIntruders.ViewModel 
+{
+    class SimpleViewModel : INotifyPropertyChanged
+    {
+        ObservableCollection<Ship> shipList;
+
+        public ObservableCollection<Ship> Ships
+        {
+            get;
+            set;
+        }
+
+        public Ship userShip { get; set; }
+
+        private Timer timer = new Timer(100);
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string PropertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new System.ComponentModel.PropertyChangedEventArgs(PropertyName));
+            }
+        }
+
+        public SimpleViewModel()
+        {
+            shipList = new ObservableCollection<Ship>();
+            shipList.Add(new Ship("Battleship.png", 10, 10, "Test1", 10));
+            shipList.Add(new Ship("Battleship.png", 10, 110, "Test2", 10));
+            shipList.Add(new Ship("Battleship.png", 10, 210, "Test3", 10));
+            shipList.Add(new Ship("Battleship.png", 100, 110, "Test4", 10));
+            
+            Ships = shipList;
+
+            timer.Elapsed += Timer_Elapsed;
+            timer.Enabled = true;
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+
+            foreach (Ship ship in shipList )
+            {
+                ship.X += 10;
+            }
+        }
+    }
+}
